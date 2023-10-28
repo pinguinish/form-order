60d6b1a935e2f71857679a0eccb4d802f25a41a4
zaferlerim <alijon.alimov.english@gmail.com>
2023-10-28 11:43:30 +0300

    Created all buttons

--- modules/libraries/uikit/example/example.dart
+++ modules/libraries/uikit/example/example.dart
@@ -1,20 +1,72 @@
-
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:uikit/const/app_palette.dart';
+import 'package:uikit/widgets/app_button.dart';
 
 void main(List<String> args) => runApp(const UIKitExample());
 
+const verticalGap20 = SizedBox(height: 20);
 
 class UIKitExample extends StatelessWidget {
   const UIKitExample({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(),
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Roboto'),
+      home: const UIKitExampleContent(),
+    );
+  }
+}
+
+class UIKitExampleContent extends StatelessWidget {
+  const UIKitExampleContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              verticalGap20,
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: AppTextButton(
+                  label: "Next step",
+                  onPressed: () {},
+                ),
+              ),
+              verticalGap20,
+              SizedBox(
+                height: 33,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: AppTextButton(
+                        label: "Add address",
+                        onPressed: () {},
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: AppTextButton(
+                        label: "Select address",
+                        onPressed: () {},
+                        backgroundColor: AppPalette.greyC4,
+                        labelStyle: const TextStyle(
+                          color: AppPalette.greyC2,
+                        ),
+                        foregroundColor: AppPalette.greyC3,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              verticalGap20
+            ],
+          ),
         ),
       ),
     );
